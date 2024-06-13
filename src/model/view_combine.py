f080c9208ccc1e90ff9bc3e53b5611a35fb09ff1
alexberian <berian@email.arizona.edu>
2024-06-13 15:55:37 -0700

    dedicated view combine file

---
+++ src/model/view_combine.py
@@ -0,0 +1,31 @@
+from torch import nn
+import torch
+
+#  import torch_scatter
+import torch.autograd.profiler as profiler
+
+
+
+
+class VanillaPixelnerfViewCombiner(nn.Module):
+
+    def combine_interleaved(self, t, inner_dims=(1,), agg_type="average"):
+        if len(inner_dims) == 1 and inner_dims[0] == 1:
+            return t
+        t = t.reshape(-1, *inner_dims, *t.shape[1:])
+        if agg_type == "average":
+            t = torch.mean(t, dim=1)
+        elif agg_type == "max":
+            t = torch.max(t, dim=1)[0]
+        else:
+            raise NotImplementedError("Unsupported combine type " + agg_type)
+        return t
+
+    def forward(self, x, combine_inner_dims, combine_type="average", **kwargs):
+        """
+        :param x (..., d_hidden)
+        :param combine_inner_dims Combining dimensions for use with multiview inputs.
+        Tensor will be reshaped to (-1, combine_inner_dims, ...) and reduced using combine_type
+        on dim 1
+        """
+        return self.combine_interleaved(x, combine_inner_dims, combine_type)
